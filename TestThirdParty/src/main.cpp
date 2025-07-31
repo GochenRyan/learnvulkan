53591409cc4a83c20b4af3070ec19001d903314e
Gaocheng <rangaocheng@outlook.com>
2025-07-31 16:05:48 +0800

    test creating a window

--- TestThirdParty/src/main.cpp
+++ TestThirdParty/src/main.cpp
@@ -24,7 +24,7 @@ int main()
 }
 #endif
 
-#if 1
+#if 0
 #include <vulkan/vulkan.h>
 #include <iostream>
 
@@ -51,6 +51,44 @@ int main() {
     std::cout << "Vulkan instance created successfully!\n";
 
     vkDestroyInstance(instance, nullptr);
+    return 0;
+}
+#endif
+
+#if 1
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
 #endif
