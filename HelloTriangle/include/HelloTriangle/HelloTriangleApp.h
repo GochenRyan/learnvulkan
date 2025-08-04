9075c76bd75ec8b3c39a06ac5153f48e59ecb05a
Gaocheng <rangaocheng@outlook.com>
2025-08-04 18:39:29 +0800

    HelloTriangleApp wip

--- HelloTriangle/include/HelloTriangle/HelloTriangleApp.h
+++ HelloTriangle/include/HelloTriangle/HelloTriangleApp.h
@@ -7,19 +7,37 @@
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApp final
 {
 public:
-    HelloTriangleApp() noexcept;
+    HelloTriangleApp() noexcept = default;
     HelloTriangleApp(const HelloTriangleApp& rhs) = delete;
     HelloTriangleApp& operator=(const HelloTriangleApp& rhs) = delete;
     HelloTriangleApp(HelloTriangleApp&& rhs) = delete;
     HelloTriangleApp& operator=(HelloTriangleApp&& rhs) = delete;
-    ~HelloTriangleApp();
+    ~HelloTriangleApp() = default;
 public:
+    void Run();
+private:
+    void initWindow();
     void initVulkan();
+    void mainLoop();
     void createInstance();
     void cleanup();
+    bool checkValidationLayerSupport();
 private:
     VkInstance instance;
     GLFWwindow* window;
--- HelloTriangle/src/HelloTriangleApp.cpp
+++ HelloTriangle/src/HelloTriangleApp.cpp
@@ -2,13 +2,42 @@
 #include <iostream>
 #include <stdexcept>
 
+void HelloTriangleApp::Run()
+{
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+}
+
+void HelloTriangleApp::initWindow()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+}
+
 void HelloTriangleApp::initVulkan()
 {
     createInstance();
 }
 
+void HelloTriangleApp::mainLoop()
+{
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+}
+
 void HelloTriangleApp::createInstance()
 {
+    if (enableValidationLayers && !checkValidationLayerSupport())
+    {
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
+
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Hello Triangle";
@@ -26,10 +55,29 @@ void HelloTriangleApp::createInstance()
 
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-    createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    std::vector<const char*> requiredExtensions;
 
-    createInfo.enabledLayerCount = 0;
+    for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+    {
+        requiredExtensions.emplace_back(glfwExtensions[i]);
+    }
+
+    requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+    createInfo.enabledExtensionCount = requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    
+    if (enableValidationLayers)
+    {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+    }
+    else
+    {
+        createInfo.enabledLayerCount = 0;
+    }
+
     if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
     {
         throw std::runtime_error("failed to create instance");
@@ -55,4 +103,32 @@ void HelloTriangleApp::cleanup()
     glfwDestroyWindow(window);
 
     glfwTerminate();
+}
+
+bool HelloTriangleApp::checkValidationLayerSupport()
+{
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers)
+    {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers)
+        {
+            if (strcmp(layerName, layerProperties.layerName) == 0)
+            {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound)
+            return false;
+    }
+
+    return true;
 }
